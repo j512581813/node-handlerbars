67d11b055128a8d033eb2f396f4c5b0addac34ef
j512581813 <512581813@qq.com>
2016-10-11 17:49:04 +0800

    添加用户注册

--- app.js
+++ app.js
@@ -8,19 +8,22 @@ var LocalStorage = require('node-localstorage').LocalStorage;
 var localStorage = LocalStorage('./scratch', Number.MAX_VALUE);
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var md5 = require('md5');
   
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(sendViewMiddleware);
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });// 设置默认布局为main
 
-app.set('port',process.env.PORT||3000);  //设置端口号
+app.set('port',process.env.PORT||8000);  //设置端口号
 app.use(express.static(__dirname + '/public')); //设置静态目录的入口
 
 
 app.engine('handlebars',handlebars.engine);// 将express模板引擎配置成handlebars
 app.set('view engine', 'handlebars');
+
+//app.set('view engine', 'html'); //用来设置加载纯静态的html界面
 
 app.set('view cache', true);  //设置缓存
 app.set('views', path.join(__dirname, 'views'));
@@ -52,28 +55,63 @@ app.get('/show',function(req,res){
 	 	res.render('show',{content:res.locals.partials.weather,image:res.locals.images});
 	});
 });
-
+app.use('/hello',function(req,res){
+		res.sendView('hello.html');;
+});
 app.use('/login',function(req,res){
 	if (!req.body) return res.sendStatus(400);
+	res.render('login',{title:"欢迎来到登陆界面！"});
+	
+});
+app.use('/loginHandle',function(req,res){
 	var username = req.body.username;
 	var password = req.body.password;
-	console.log(username)
 	var conn = mysql.createConnection(settings.db);
-	if(username&&password){
-		console.log(222222);
+	if(username!=""&&password!==""){
 		conn.query("select count(*) as count from user WHERE username='"+username +"'", function(err, datas) {
-			if (err) throw err;
-			if(datas[0].count ===1){
-				//title = "这是登陆界面！";
-				console.log("select success")
-				res.send('hello'+req.body.username);
-			}else{
-				res.send("error",{error:"您输入的用户名或密码有误！"});
-			}
-			res.render('login');
-		});
-  	}
+		if (err) throw err;
+		if(datas[0].count ===1){
+			console.log("select success")
+			res.send('hello,'+req.body.username);
+		}else{
+			console.log("用户名或者密码有误！");
+		}
+		
+	});
+	}else{
+		console.log("用户名或者密码不能为空！");
+	}
+ 
+
+});
+
+app.use('/register',function(req,res){
+	if (!req.body) return res.sendStatus(400);
+	res.render('register',{title:"欢迎来到注册界面！"});
 })
+
+app.use('/registerHandle',function(req,res){
+	var username = req.body.username;
+	var password = req.body.pwd;
+	var checkPwd = req.body.checkPwd;
+	var conn = mysql.createConnection(settings.db);
+	if(username!=""&&password!==""&&checkPwd!==""){
+		if(password == checkPwd){
+			var str = md5(password);
+			conn.query('insert into user (username,password ) values ("'+ username +'" ,"' +str+'")', function(err){
+			if(err) return false;
+		    	res.send('hello,'+req.body.username);
+			});
+		}else{
+			
+			res.render('register',{title:"密码和重复密码必须一致！"});
+		}
+	}else{
+		
+		res.render('register',{title:"用户名或者密码不能为空！"});
+	}
+})
+
 app.use(function(req,res,next){
 	res.status(404);
 	res.render('404');
@@ -94,3 +132,10 @@ app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
 });
+//关于静态界面的显示静态文件的公共用方法
+// function sendViewMiddleware(req, res, next) {
+//     res.sendView = function(view) {
+//         return res.sendFile(__dirname + "/views/" + view);
+//     }
+//     next();
+// }
